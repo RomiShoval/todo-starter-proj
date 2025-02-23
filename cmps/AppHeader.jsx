dea6b8518771af2c0b8e4a77115358a772cfdde6
romi shoval <romishoval14@gmail.com>
2025-02-23 17:50:39 +0200

    added progress bar

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,6 +1,7 @@
 const { useState } = React
 const { Link, NavLink } = ReactRouterDOM
 const { useNavigate } = ReactRouter
+const { useSelector } = ReactRedux
 
 import { userService } from '../services/user.service.js'
 import { UserMsg } from "./UserMsg.jsx"
@@ -11,6 +12,11 @@ import { showErrorMsg } from '../services/event-bus.service.js'
 export function AppHeader() {
     const navigate = useNavigate()
     const [user, setUser] = useState(userService.getLoggedinUser())
+
+    const todos = useSelector(state => state.toDoModule.todos)
+    const completedTodos = todos.filter(todo => todo.isDone).length
+    const totalTodos = todos.length
+    const precentageTodos = totalTodos ? Math.round((completedTodos/totalTodos)*100) : 0
     
     function onLogout() {
         userService.logout()
@@ -47,6 +53,13 @@ export function AppHeader() {
                     <NavLink to="/todo" >Todos</NavLink>
                     <NavLink to="/dashboard" >Dashboard</NavLink>
                 </nav>
+            </section>
+
+            <section className="progress-bar-container">
+                <label>Progress : {precentageTodos}%</label>
+                <div className="progress-bar">
+                    <div className="progress" style={{ width: `${precentageTodos}%` }}></div>
+                </div>
             </section>
             <UserMsg />
         </header>
